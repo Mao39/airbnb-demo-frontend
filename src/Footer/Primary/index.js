a6805bc549e1372ab5280cae31258fb617be00f2
Mao39 <steallermail@gmail.com>
2018-01-06 14:38:20 +0300

    remove component-wrap on Primary

--- src/Footer/Primary/index.js
+++ src/Footer/Primary/index.js
@@ -3,17 +3,13 @@ import styled from "styled-components";
 import Selects from "./Selects";
 import Nav from "./Nav";
 
-const Service = styled.div``;
-
 export default function() {
   return (
-    <Service>
-      <div className="container">
-        <div className="row">
-          <Selects />
-          <Nav />
-        </div>
+    <div className="container">
+      <div className="row">
+        <Selects />
+        <Nav />
       </div>
-    </Service>
+    </div>
   );
 }
