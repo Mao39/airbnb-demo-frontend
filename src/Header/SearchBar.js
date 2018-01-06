abaefe145c9cee3651da04ae24c0ba063dc6bb5d
Mao39 <steallermail@gmail.com>
2018-01-06 17:41:06 +0300

    Header -> more friendly adaptive

--- src/Header/SearchBar.js
+++ src/Header/SearchBar.js
@@ -13,28 +13,39 @@ const SearchBar = styled.div`
 const Button = styled.span`
   position: absolute;
   top: 50%;
-  left: 16.2px;
+  left: 14px;
   transform: translateY(-50%);
   display: inline-block;
-  width: 22px;
-  height: 22px;
+  width: 15px;
+  height: 16px;
   border: none;
   background: url(${search}) no-repeat center center;
+  background-size: cover;
   cursor: pointer;
+
+  @media (min-width: 320px) {
+    width: 22px;
+    height: 22px;
+  }
 `;
 
 const Input = styled.input`
   width: 100%;
   height: 48px;
-  padding-left: 52px;
+  padding-left: 35px;
   border: 1px solid rgba(72, 72, 72, 0.2);
   border-radius: 4px;
   font-family: Circular, Helvetica Neue, Helvetica, Arial, sans-serif;
-  font-size: 16px;
+  font-size: 14px;
   line-height: 24px;
   font-weight: 400;
   color: #383838;
   box-shadow: 0px 2px 4px rgba(72, 72, 72, 0.08);
+
+  @media (min-width: 320px) {
+    padding-left: 52px;
+    font-size: 16px;
+  }
 `;
 
 export default function() {
