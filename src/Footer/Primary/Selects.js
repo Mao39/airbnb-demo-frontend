5dc7b0924f2555a7957abbc3bac31f79055950d0
Mao39 <steallermail@gmail.com>
2018-01-06 14:48:33 +0300

    remove extra styles, Selects -> MobileFirst, change of attributes

--- src/Footer/Primary/Selects.js
+++ src/Footer/Primary/Selects.js
@@ -8,39 +8,37 @@ const Select = styled.select`
   padding: 12px 16px;
   border: 1px solid rgba(72, 72, 72, 0.2);
   border-radius: 4px;
-  background: url(${arrow}) no-repeat right 20px top 20px;
+  background: url(${arrow}) no-repeat;
+  background-position: right 10px top 16px;
+  background-size: 13px;
   appearance: none;
   cursor: pointer;
 
-  @media (max-width: 991px) {
-    font-size: 13px;
-    background-size: 13px;
-    background: url(${arrow}) no-repeat right 10px top 16px;
-  }
-
-  @media (max-width: 576px) {
-    font-size: 18px;
+  @media (min-width: 991px) {
+    background-position: right 20px top 20px;
+    backround-size: auto;
   }
 `;
 
 const Option = styled.option`
-  font-size: 18px;
+  font-size: 13px;
   line-height: 21px;
   font-weight: lighter;
   color: #383838;
+  @media (min-width: 576px) {
+    font-size: 18px;
+  }
 `;
 
 export default function() {
   return (
     <div className="col-md-3 col-sm-3 col-xs-12">
-      <Select>
-        <Option defaultValue="English">English</Option>
+      <Select defaultValue="English">
+        <Option value="English">English</Option>
         <Option value="Русский">Русский</Option>
       </Select>
-      <Select>
-        <Option defaultValue="United States dollar">
-          United States dollar
-        </Option>
+      <Select defaultValue="United States dollar">
+        <Option value="United States dollar">United States dollar</Option>
         <Option value="Российский рубль">Российский рубль</Option>
       </Select>
     </div>
