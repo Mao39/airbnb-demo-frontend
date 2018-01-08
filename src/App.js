e8c84d6e238be5acf5bca28e3bef3da41bca1874
Mao39 <steallermail@gmail.com>
2018-01-08 18:36:23 +0200

    install google-map-react, react-router-dom | ignore *.map | new Homes directory

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css";
 import "flexboxgrid2";
 import "./index.css";
 import Header from "./Header";
 import Main from "./Main";
+import Homes from "./Homes";
 import Footer from "./Footer";
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Header />
-        <Main />
-        <Footer />
-      </React.Fragment>
+      <BrowserRouter>
+        <React.Fragment>
+          <Header />
+          <Route path="/" exact component={Main} />
+          <Route path="/homes" component={Homes} />
+          <Footer />
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/Homes/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import Card from "../Main/Homes/Card";
+
+const Main = styled.main``;
+
+export default function() {
+  return (
+    <Main>
+      <h1> Hello, World</h1>
+      <Card
+        price={82}
+        numberBeds="9 beds"
+        houseType="Entrie house"
+        numberReview={97}
+        hostType="Superhost"
+      >
+        La Salentina, see, nature & relax
+      </Card>
+    </Main>
+  );
+}
