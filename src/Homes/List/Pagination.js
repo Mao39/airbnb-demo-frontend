c2100cfdd4d24a028ceb1ac343d572303337b97f
Mao39 <steallermail@gmail.com>
2018-01-13 14:25:05 +0300

    Switch font-weight bold -> 600 on Homes/Footer (Pagination)

--- src/Homes/List/Pagination.js
+++ src/Homes/List/Pagination.js
@@ -10,7 +10,7 @@ const Pagination = styled.div`
   font-family: Circular, Helvetica Neue, Helvetica, Arial, sans-serif;
   font-size: 16px;
   line-height: 19px;
-  font-weight: bold;
+  font-weight: 600;
   text-align: center;
 `;
 
@@ -62,7 +62,7 @@ const Next = styled.button`
 
 const Total = styled.p`
   margin-top: 20px;
-  font-weight: bold;
+  font-weight: 600;
   color: #383838;
 `;
 
