10843bb2492703623b6ca4cded9d8fb8cd922f15
Mao39 <steallermail@gmail.com>
2018-01-13 10:24:37 +0300

    remove useless grid classes -> Header

--- src/Header/index.js
+++ src/Header/index.js
@@ -61,7 +61,7 @@ export default () => {
     <Header>
       <div className="container">
         <div className="row">
-          <div className="col-xs-2 col-sm-1 col-md-1">
+          <div className="col-xs-2 col-sm-1">
             <LogoLink to="/">
               <Logo src={logo} alt="Logo" />
               <Dropdown />
