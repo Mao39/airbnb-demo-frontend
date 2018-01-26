1e38cd2a2091e43340b2eab7da1b34a9203c3a6f
Mao39 <steallermail@gmail.com>
2018-01-26 19:21:47 +0200

    Guests -> Change the order of imports | Change the order of ads | Edit function names

--- src/Homes/Filters/Guests.js
+++ src/Homes/Filters/Guests.js
@@ -1,46 +1,71 @@
 import React from 'react';
 import styled from 'styled-components';
 import ScrollLock from 'react-scrolllock';
+
+import plus from '../../UI/plus.svg';
 import cross from '../../UI/cross.svg';
 import minus from '../../UI/minus.svg';
-import plus from '../../UI/plus.svg';
 
-const Filter = styled.aside`
-  display: inline-block;
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  background-color: #fff;
-  z-index: 3;
+const Add = styled.button`
+  position: relative;
+  width: 32px;
+  height: 32px;
+  border: 1px solid #008489;
+  border-radius: 50%;
+  background: url(${plus}) no-repeat center;
+  background-size: 10px 10px;
+  cursor: pointer;
+`;
+
+const Age = styled.span`
+  margin-top: 6px;
+  font-family: Circular, Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  line-height: 16px;
+  font-weight: lighter;
+  color: #383838;
+`;
+
+const Amount = styled.span`
+  margin-right: 18px;
+  margin-left: 18px;
+  font-family: Circular, Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 18px;
+  line-height: 21px;
+  font-weight: lighter;
+  color: #383838;
+`;
+
+const Apply = styled.button`
+  display: none;
+  border: none;
+  font-family: Circular, Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  line-height: 19px;
+  font-weight: 600;
+  color: #008489;
+  background: transparent;
+  cursor: pointer;
 
   @media (min-width: 576px) {
-    position: absolute;
-    top: 40px;
-    left: 0;
-    height: 290px;
-    width: 326px;
-    padding: 32px 24px;
-    border: 1px solid rgba(72, 72, 72, 0.2);
-    border-radius: 4px;
-    box-shadow: 0 2px 4px rgba(72, 72, 72, 0.08);
+    display: inline-block;
   }
 `;
 
-const Wrap = styled.div`
-  position: relative;
-  display: inline-block;
-`;
-
-const Overlay = styled.div`
-  position: fixed;
-  top: 137px;
+const Bottom = styled.div`
+  position: absolute;
   right: 0;
   bottom: 0;
   left: 0;
-  z-index: 2;
-  background: rgba(255, 255, 255, 0.8);
+  display: flex;
+  justify-content: space-between;
+  padding: 8px;
+  box-shadow: 0 -1px #d5d5d5;
+
+  @media (min-width: 576px) {
+    padding: 26px;
+    box-shadow: none;
+  }
 `;
 
 const Btn = styled.button`
@@ -65,18 +90,25 @@ const Btn = styled.button`
   }
 `;
 
-const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 40px;
-  height: 48px;
-  padding: 0 8px;
-  box-shadow: 0 0.5px rgba(72, 72, 72, 0.3);
+const Cancel = styled.button`
+  display: none;
+  border: none;
+  font-family: Circular, Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  line-height: 19px;
+  font-weight: 600;
+  color: #636363;
+  background: transparent;
+  cursor: pointer;
 
   @media (min-width: 576px) {
-    display: none;
+    display: inline-block;
   }
+`;
+
+const Control = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const Caption = styled.span`
@@ -96,29 +128,41 @@ const Exit = styled.button`
   cursor: pointer;
 `;
 
-const Reset = styled.button`
-  padding: 0;
-  font-family: Circular, Helvetica Neue, Helvetica, Arial, sans-serif;
-  font-size: 14px;
-  line-height: 16px;
-  font-weight: 600;
-  color: #0f7276;
-  border: none;
-  background: transparent;
-  cursor: pointer;
+const Filter = styled.aside`
+  display: inline-block;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: #fff;
+  z-index: 3;
+
+  @media (min-width: 576px) {
+    position: absolute;
+    top: 40px;
+    left: 0;
+    height: 290px;
+    width: 326px;
+    padding: 32px 24px;
+    border: 1px solid rgba(72, 72, 72, 0.2);
+    border-radius: 4px;
+    box-shadow: 0 2px 4px rgba(72, 72, 72, 0.08);
+  }
 `;
 
-const People = styled.div`
+const Header = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 30px;
+  margin-bottom: 40px;
+  height: 48px;
   padding: 0 8px;
-`;
+  box-shadow: 0 0.5px rgba(72, 72, 72, 0.3);
 
-const Type = styled.span`
-  display: flex;
-  flex-direction: column;
+  @media (min-width: 576px) {
+    display: none;
+  }
 `;
 
 const Man = styled.span`
@@ -130,21 +174,25 @@ const Man = styled.span`
   text-transform: capitalize;
 `;
 
-const Age = styled.span`
-  margin-top: 6px;
-  font-family: Circular, Helvetica Neue, Helvetica, Arial, sans-serif;
-  font-size: 14px;
-  line-height: 16px;
-  font-weight: lighter;
-  color: #383838;
+const Overlay = styled.div`
+  position: fixed;
+  top: 137px;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 2;
+  background: rgba(255, 255, 255, 0.8);
 `;
 
-const Control = styled.div`
+const People = styled.div`
   display: flex;
+  justify-content: space-between;
   align-items: center;
+  margin-bottom: 30px;
+  padding: 0 8px;
 `;
 
-const Remove = styled.button`
+const Reduce = styled.button`
   position: relative;
   width: 32px;
   height: 32px;
@@ -157,41 +205,16 @@ const Remove = styled.button`
   cursor: ${props => (props.disabled ? 'default' : 'pointer')};
 `;
 
-const Add = styled.button`
-  position: relative;
-  width: 32px;
-  height: 32px;
-  border: 1px solid #008489;
-  border-radius: 50%;
-  background: url(${plus}) no-repeat center;
-  background-size: 10px 10px;
-  cursor: pointer;
-`;
-
-const Amount = styled.span`
-  margin-right: 18px;
-  margin-left: 18px;
+const Reset = styled.button`
+  padding: 0;
   font-family: Circular, Helvetica Neue, Helvetica, Arial, sans-serif;
-  font-size: 18px;
-  line-height: 21px;
-  font-weight: lighter;
-  color: #383838;
-`;
-
-const Bottom = styled.div`
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  display: flex;
-  justify-content: space-between;
-  padding: 8px;
-  box-shadow: 0 -1px #d5d5d5;
-
-  @media (min-width: 576px) {
-    padding: 26px;
-    box-shadow: none;
-  }
+  font-size: 14px;
+  line-height: 16px;
+  font-weight: 600;
+  color: #0f7276;
+  border: none;
+  background: transparent;
+  cursor: pointer;
 `;
 
 const Save = styled.button`
@@ -212,36 +235,14 @@ const Save = styled.button`
   }
 `;
 
-const Cancel = styled.button`
-  display: none;
-  border: none;
-  font-family: Circular, Helvetica Neue, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 19px;
-  font-weight: 600;
-  color: #636363;
-  background: transparent;
-  cursor: pointer;
-
-  @media (min-width: 576px) {
-    display: inline-block;
-  }
+const Characteristics = styled.span`
+  display: flex;
+  flex-direction: column;
 `;
 
-const Apply = styled.button`
-  display: none;
-  border: none;
-  font-family: Circular, Helvetica Neue, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 19px;
-  font-weight: 600;
-  color: #008489;
-  background: transparent;
-  cursor: pointer;
-
-  @media (min-width: 576px) {
-    display: inline-block;
-  }
+const Wrap = styled.div`
+  position: relative;
+  display: inline-block;
 `;
 
 const formatGuestsLabel = (filterLabel, adults, children, infants) => {
@@ -257,21 +258,21 @@ const formatGuestsLabel = (filterLabel, adults, children, infants) => {
   return filterLabel;
 };
 
-const ShowOverlay = (isOpen, onCloseFilter) => isOpen && <Overlay onClick={onCloseFilter} />;
+const ShowOverlay = (isOpen, onClose) => isOpen && <Overlay onClick={onClose} />;
 
 const ShowScrollLock = isOpen =>
   !matchMedia('(min-width: 576px)').matches && isOpen && <ScrollLock />;
 
 const Guest = props => (
   <People>
-    <Type>
+    <Characteristics>
       <Man>{props.type}</Man>
       <Age>{props.age}</Age>
-    </Type>
+    </Characteristics>
     <Control>
-      <Remove
+      <Reduce
         onClick={() => props.reduce(props.type)}
-        disabled={props.switchDisableButton(props.type)}
+        disabled={props.switchDisableReduce(props.type)}
       />
       <Amount>{props.amount}</Amount>
       <Add onClick={() => props.add(props.type)} />
@@ -287,9 +288,9 @@ export default class Guests extends React.Component {
     isApply: false,
   };
 
-  onCloseFilter = () => {
+  onClose = () => {
     this.resetSelection();
-    this.props.onCloseFilter();
+    this.props.onClose();
   };
 
   onApply = () => {
@@ -306,7 +307,7 @@ export default class Guests extends React.Component {
     this.props.switchOpeningFilter(this.props.children);
   };
 
-  switchDisableButton = (type) => {
+  switchDisableReduce = (type) => {
     if (this.state[type] > 1) return false;
     if (this.state[type] > 0 && type !== 'adults') return false;
     return true;
@@ -342,7 +343,7 @@ export default class Guests extends React.Component {
           {isOpen && (
             <Filter>
               <Header>
-                <Exit onClick={this.onCloseFilter} />
+                <Exit onClick={this.onClose} />
                 <Caption>Guests</Caption>
                 <Reset onClick={this.resetSelection}>Reset</Reset>
               </Header>
@@ -351,7 +352,7 @@ export default class Guests extends React.Component {
                 amount={adults}
                 add={this.add}
                 reduce={this.reduce}
-                switchDisableButton={this.switchDisableButton}
+                switchDisableReduce={this.switchDisableReduce}
               />
               <Guest
                 type="children"
@@ -359,7 +360,7 @@ export default class Guests extends React.Component {
                 amount={children}
                 add={this.add}
                 reduce={this.reduce}
-                switchDisableButton={this.switchDisableButton}
+                switchDisableReduce={this.switchDisableReduce}
               />
               <Guest
                 type="infants"
@@ -367,20 +368,20 @@ export default class Guests extends React.Component {
                 amount={infants}
                 add={this.add}
                 reduce={this.reduce}
-                switchDisableButton={this.switchDisableButton}
+                switchDisableReduce={this.switchDisableReduce}
               />
               <Bottom>
                 <Save onClick={this.onApply}>Save</Save>
                 {adults > 1 || children || infants ? (
                   <Cancel onClick={this.resetSelection}>Reset</Cancel>
                 ) : (
-                  <Cancel onClick={this.onCloseFilter}>Cancel</Cancel>
+                  <Cancel onClick={this.onClose}>Cancel</Cancel>
                 )}
                 <Apply onClick={this.onApply}>Apply</Apply>
               </Bottom>
             </Filter>
           )}
-          {ShowOverlay(isOpen, this.onCloseFilter)}
+          {ShowOverlay(isOpen, this.onClose)}
           {ShowScrollLock(isOpen)}
         </Wrap>
       </React.Fragment>
